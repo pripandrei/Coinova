b93f616cf0751e9cabb52c775552722edf0ff260
Andrei <pripandrei@gmail.com>
2026-03-14 18:20:58 +0200

    added Coin Model

--- CryptoApp/Models/Coin.swift
+++ CryptoApp/Models/Coin.swift
@@ -5,9 +5,106 @@
 //  Created by Andrei Pripa on 3/12/26.
 //
 
-struct Coin: Codable
+// API : https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h
+
+//JSON Data :
+//{
+//    "id": "bitcoin",
+//    "symbol": "btc",
+//    "name": "Bitcoin",
+//    "image": "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+//    "current_price": 70599,
+//    "market_cap": 1411261512921,
+//    "market_cap_rank": 1,
+//    "fully_diluted_valuation": 1411261512921,
+//    "total_volume": 35787731553,
+//    "high_24h": 72105,
+//    "low_24h": 70417,
+//    "price_change_24h": -1431.18993402796,
+//    "price_change_percentage_24h": -1.98693,
+//    "market_cap_change_24h": -32942885830.178,
+//    "market_cap_change_percentage_24h": -2.28104,
+//    "circulating_supply": 20002003,
+//    "total_supply": 20002003,
+//    "max_supply": 21000000,
+//    "ath": 126080,
+//    "ath_change_percentage": -44.00461,
+//    "ath_date": "2025-10-06T18:57:42.558Z",
+//    "atl": 67.81,
+//    "atl_change_percentage": 104014.38172,
+//    "atl_date": "2013-07-06T00:00:00.000Z",
+//    "roi": null,
+//    "last_updated": "2026-03-14T15:54:52.900Z",
+//    "sparkline_in_7d": {
+//      "price": [67991.1035699002, 67916.103289176, 68019.1601918745, 67899.1508484934, 67801.0163226986 ...]
+//    },
+//    "price_change_percentage_24h_in_currency": -1.98693284435742
+//  }, ...
+
+import Foundation
+
+struct Coin: Identifiable, Codable
 {
-  
+    let id: String
+    let symbol: String
+    let name: String
+    let image: String
+    let currentPrice: Double
+    let marketCap: Double?
+    let marketCapRank: Int?
+    let fullyDilutedValuation: Double?
+    let totalVolume: Double?
+    let high24h: Double?
+    let low24h: Double?
+    let priceChange24h: Double?
+    let priceChangePercentage24h: Double?
+    let marketCapChange24h: Double?
+    let marketCapChangePercentage24h: Double?
+    let circulatingSupply: Double?
+    let totalSupply: Double?
+    let maxSupply: Double?
+    let ath: Double?
+    let athChangePercentage: Double?
+    let athDate: String?
+    let atl: Double?
+    let atlChangePercentage: Double?
+    let atlDate: String?
+    let lastUpdated: String?
+    let sparklineIn7d: SparklineIn7d?
+    let priceChangePercentage24hInCurrency: Double?
+
+    enum CodingKeys: String, CodingKey
+    {
+        case id, symbol, name, image
+        case currentPrice                       = "current_price"
+        case marketCap                          = "market_cap"
+        case marketCapRank                      = "market_cap_rank"
+        case fullyDilutedValuation              = "fully_diluted_valuation"
+        case totalVolume                        = "total_volume"
+        case high24h                            = "high_24h"
+        case low24h                             = "low_24h"
+        case priceChange24h                     = "price_change_24h"
+        case priceChangePercentage24h           = "price_change_percentage_24h"
+        case marketCapChange24h                 = "market_cap_change_24h"
+        case marketCapChangePercentage24h       = "market_cap_change_percentage_24h"
+        case circulatingSupply                  = "circulating_supply"
+        case totalSupply                        = "total_supply"
+        case maxSupply                          = "max_supply"
+        case ath
+        case athChangePercentage                = "ath_change_percentage"
+        case athDate                            = "ath_date"
+        case atl
+        case atlChangePercentage                = "atl_change_percentage"
+        case atlDate                            = "atl_date"
+        case lastUpdated                        = "last_updated"
+        case sparklineIn7d                      = "sparkline_in_7d"
+        case priceChangePercentage24hInCurrency = "price_change_percentage_24h_in_currency"
+    }
+}
+
+struct SparklineIn7d: Codable
+{
+    let price: [Double]
 }
 
 
